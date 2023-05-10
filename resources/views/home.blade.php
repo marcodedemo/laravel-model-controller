696eff6e179fb293506b272f85b9b319b93145f2
marcodedemo <marco.dedemo@gmail.com>
2023-05-10 13:55:21 +0200

    feat: connected database

---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+
+
+@extends('layouts/main-layout')
+
+
+
+@section('content')
+
+<div id="home-container" class="container d-flex justify-content-center align-items-center">
+    
+    
+</div>
+
+@endsection
